0453f9cece3e115cc53b10a23af75c014a957a1a
Eli Vermeulen <eli.vermeulen2002@gmail.com>
2024-08-08 21:17:50 +0200

    Add colorcode to answers and make it so that the button and input get disabled

--- static/js/script.js
+++ static/js/script.js
@@ -6,8 +6,8 @@ function checkTranslation(button) {
     // Get the translation from the hidden span
     const translation = row.querySelector('.translation').innerText;
     
-    // Get the user input from the input box
-    const input = row.querySelector('.translation-input').value;
+    const inputField = row.querySelector('.translation-input');
+    const input = inputField.value;
     
     // Get the button element to update its text
     const checkButton = row.querySelector('.button');
@@ -16,10 +16,30 @@ function checkTranslation(button) {
     if (input === translation) {
         checkButton.innerText = 'Correct'; // Change button text to "Correct"
         checkButton.classList.remove('is-primary'); // Optionally, remove primary styling
+        checkButton.classList.remove('is-danger');
         checkButton.classList.add('is-success'); // Optionally, add success styling
+        checkButton.disabled = true;
+        inputField.disabled = true;
+        japanese_word = row.parentElement.querySelector(".japanese-word");
+        japanese_word.classList.remove("has-background-danger");
+        japanese_word.classList.add("has-background-primary")
     } else {
         checkButton.innerText = 'Incorrect'; // Change button text to "Incorrect"
         checkButton.classList.remove('is-primary'); // Optionally, remove primary styling
         checkButton.classList.add('is-danger'); // Optionally, add danger styling
     }
 }
+
+
+function clearInputFields() {
+    // Select all input fields with the class 'translation-input'
+    const inputs = document.querySelectorAll('.translation-input');
+    
+    // Iterate through the inputs and clear their value
+    inputs.forEach(input => {
+        input.value = ''; // Clear the input field value
+        input.disabled = false; // Ensure the input is enabled
+    });
+}
+
+addEventListener('DOMContentLoaded', clearInputFields());
